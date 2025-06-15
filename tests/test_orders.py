8a7492aca56a93246fceb2d410161d0fd738818a
Egor Nikiforov <shutrez101@gmail.com>
2025-06-15 18:29:42 +0300

    dip 2 try 2

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -4,6 +4,20 @@ from helpers.data import Order
 from helpers.data import Responses
 from helpers.utils import OrderApiHelper
 
+@pytest.fixture
+def create_order(create_user):
+    token = create_user['token']
+    ingredients = [Order.bun, Order.main, Order.sauce]
+    order = OrderApiHelper.create_order(ingredients, token)
+    return order
+
+@pytest.fixture
+def create_orders(create_user):
+    token = create_user['token']
+    # Создаем два заказа
+    order_1 = OrderApiHelper.create_order([Order.main], token)
+    order_2 = OrderApiHelper.create_order([Order.bun], token)
+    return [order_1, order_2]
 
 class TestCreateOrder:
 
@@ -11,11 +25,11 @@ class TestCreateOrder:
     def test_create_new_order_no_authorization(self):
         ingredients = [Order.bun, Order.main, Order.sauce]
         order = OrderApiHelper.create_order(ingredients)
-        assert order.status_code != 200 and order.json()['success'] != True
+        assert order.status_code != 200 and order.json()['success'] is not True
 
     @allure.title('Создание заказа с авторизацией и с валидными ингредиентами.')
     def test_create_new_order_with_authorization(self, create_order):
-        assert create_order.status_code == 200 and create_order.json()['success'] == True
+        assert create_order.status_code == 200 and create_order.json()['success'] is True
 
     @allure.title('Создание заказа с авторизацией без ингредиентов/с невалидными ингредиентами.')
     @pytest.mark.parametrize("ingredients, code", [([], 400), (["invalid"], 500)])
@@ -31,14 +45,11 @@ class TestGetOrder:
         orders = OrderApiHelper.request_user_orders_list()
         assert orders.status_code == 401 and orders.json() == Responses.CODE_401_GET_ORDERS_NO_AUTH
 
-    @allure.title('Получение заказов пользователя c авторизацией.')
-    def test_get_user_orders_with_authorization(self, create_user):
-        token = create_user['token']
-
-        order_1 = OrderApiHelper.create_order(Order.main, token)
-        assert order_1.status_code == 200
-        order_2 = OrderApiHelper.create_order(Order.bun, token)
-        assert order_2.status_code == 200
-
-        orders_list = OrderApiHelper.request_user_orders_list(token)
+    @allure.title('Получение заказов пользователя с авторизацией.')
+    def test_get_user_orders_with_authorization(self, create_orders):
+        orders_list = OrderApiHelper.request_user_orders_list(create_orders[0]['token'])
         assert orders_list.status_code == 200
+
+        created_order_ids = [order['id'] for order in create_orders]
+        response_order_ids = [order['id'] for order in orders_list.json()]
+        assert all(order_id in response_order_ids for order_id in created_order_ids)
